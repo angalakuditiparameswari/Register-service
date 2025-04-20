b07421cacf523cbbf66411934eea61bf678e167c
angalakuditiparameswari <angalakuditiparameswari@gmail.com>
2025-04-20 12:14:58 +0530

    included JsonInclude

--- src/main/java/com/example/registerservice/dto/UserResponse.java
+++ src/main/java/com/example/registerservice/dto/UserResponse.java
@@ -4,12 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserResponse {
     private Long id;
     private String username;
+    private String password;
 }
